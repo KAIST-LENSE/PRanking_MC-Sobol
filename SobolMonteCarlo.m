65bd3bfd1a341790346031c84dbd7facac8e008b
Jeehwan Lee (Steve) <stevelee@kaist.ac.kr>
2020-04-29 10:30:54 +0900

    Fixed 1st order and Total Sobol calculations
    Uses the Classical 1st Order and Total Sobol method, which can be optionally improved to Kucherenko's method if need be. Also added GUI  elements such as Progress Bar as well as warning prompts for large simulation sizes

--- SobolMonteCarlo.m
+++ SobolMonteCarlo.m
@@ -8,13 +8,27 @@ close all;
 clc;
 tic;
 format longG;
-progress = waitbar(0, '1', 'Name', 'Running PR-MC-Sobol...');
+progress = waitbar(0, 'Running...', 'Name', 'Running PR-MC-Sobol...');
+set(findall(progress), 'Units', 'Normalized')
+set(progress, 'Position', [0.25, 0.4, 0.18, 0.12])
 total_steps = 1000;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% [1] Initialize Parameters and Spaces
 % (a) Define # of Model Evaluation (# of MC sampling of Parameter Sets)
-n_sim = 10000;
-
+n_sim = 55000;
+export = 1;
+if n_sim > 50000
+    simwarn = questdlg('WARNING: For n_sim >50,000 there could be issues with exporting results via xlswrite. Disable xlswrite?',...
+        'WARNING',...
+        'No','Yes','Yes');
+    switch simwarn
+        case 'Yes'
+            export = 0;
+        case 'No'
+            export = 1;
+    end
+end
+            
 % (b) Initialize Parameter Values for Key Parameters with kp
 %     The number of elements in kp should correspond to # of key parameters 
 %     that are considered in the model. If the # of key params of the model 
@@ -34,6 +48,7 @@ c_ParSpace = [];               % Space for Complementary Parameters to kp
 
 %%Progress Bar%%
 waitbar(20/total_steps, progress, 'Generating Parameter Spaces...');
+
 
 
 %% [2] Populate Parameter Space via MC Simulation
@@ -114,167 +129,175 @@ c_ParSpace(:,10) = random(f_x10, n_sim, 1);
 waitbar(100/total_steps, progress, 'Initializing Monte Carlo Simulations');
 
 
+
 %% [3] Process Parameter Space for CCU Evaluation
 % (a) Determine the # of CCU Model Outputs (# of Evaluation Metrics)
 n_out = size(CCUS_Biocrude(kp),2);
 
 % (b) Create containers for Evaluated Model Outputs
 % NOTE: eval_P and eval_C are n_sim x n_out x n_kp matrix. 
-outputs = zeros(n_sim, n_out);   % Evaluated outputs with MC sampled inputs
-eval_P = zeros(n_sim, n_out);    % kp from ParSpace, 1st Sobol
-eval_C = zeros(n_sim, n_out);    % kp from ComplementarySpace, Total Sobol
+fx = zeros(n_sim, n_out);  % Evaluated outputs with all Params from ParSpace
+fx_P = zeros(n_sim, n_out);% Evaluated outputs with i from ParSpace, ~i from c_ParSpace
+fx_C = zeros(n_sim, n_out);% Evaluated outputs with i from c_ParSpacek, ~i from ParSpace
 
-% (c) Using Monte Carlo sampled parameter values in (2a), evaluate model
+% (c) Evaluate Model from Monte Carlo Sampled Inputs (ParSpace)
 for i = 1:n_sim
     % Each Parameter Set is a Row in the ParSpace matrix
     Parameter_Set = ParSpace(i,:); 
-    outputs(i,:) = CCUS_Biocrude(Parameter_Set);
+    fx(i,:) = CCUS_Biocrude(Parameter_Set);
 end
 
-% (d) Compute Factors for 1st Order and Total Sobol
+% (d) Generate Function Output Space based on i and ~i
 for i = 1:n_sim
     for j = 1:n_kp
-        %%%% 1st Order Sobol Factors
+        %%%% fx_P = f(x_ik, x'_~ik)
         % Loop through all Parameters by indexing with j. For a certain 
         % parameter of interest, (lets say 3rd parameter, j=3) kp becomes 
         % a n_sim x n_p matrix where the column values for the parameter of
         % interest (3rd column) are assigned from the Parameter Space. The 
         % columns for remaining parameters (j=/=3) are populated with 
-        % values from the Complementary Space. 
+        % values from the Complementary Space. This input parameter set
+        % (x_ik, x'_~ik) is evaluated to compute D_i (partial variance for 
+        % parameter i)
         kp = [c_ParSpace(i,1:j-1), ParSpace(i,j), c_ParSpace(i,j+1:n_kp)];
-        eval_P(i,:,j) = CCUS_Biocrude(kp);
-        %%%% Total Sobol Factors
+        fx_P(i,:,j) = CCUS_Biocrude(kp);
+        %%%% fx_C = f(x'_ik, x_~ik)
         % Loop through all Parameters by indexing with j. For a certain 
         % parameter of interest, (lets say 5th parameter, j=5) kp becomes a
         % n_sim x n_p matrix where the column values for the parameter of 
         % interest (5th column) are assigned from the Complementary Space. 
         % The columns for remaining parameters (j=/=5) are populated with 
-        % values from the Parameter Space. 
+        % values from the Parameter Space. This input parameter set
+        % (x'_ik, x_~ik) is evaluated to compute D_Tot_i (total variance 
+        % involving parameter i)
         kp = [ParSpace(i,1:j-1), c_ParSpace(i,j), ParSpace(i,j+1:n_kp)];
-        eval_C(i,:,j) = CCUS_Biocrude(kp);
+        fx_C(i,:,j) = CCUS_Biocrude(kp);
     end
     %%Progress Bar%%
     waitbar((100 + ((i/n_sim)*820))/total_steps, progress,...
-             sprintf('Processing %d of %d Simulations', i, n_sim));
+    sprintf('Processing %d of %d Simulations', i, n_sim));
 end
 %%Progress Bar%%
 waitbar(920/total_steps, progress, 'Computing Sobol Variances');
 
 
+
 %% [4] Computing Variances
 % (a) Initialize Integrals and Total Variances
-% Initialize Integral of Model Outputs via ANOVA decomposition 
+% Initialize the Integral of Model Outputs (f0^2) 
 f0 = zeros(1, n_out);            
-% Initialize Total Variance of Model Outputs initialized to zero 
+% Initialize Total Variance of Model Outputs 
 D = zeros(1, n_out);            
 
-% (b) Estimating Total Output Variance, D, using MC Model Outputs
-for i = 1:n_out
+% (b) Compute the Average of Model Outputs, f0
+for i = 1:n_out 
     for j = 1:n_sim
-        % The Integral of Model Outputs is estimated by 1/N*SIGMA(f(x_k)) 
-        % from k=1 to N, AKA, the average of all Model Output values from 
-        % the Monte Carlo simulations
-        % EX: For j=1 (1st CCU model output), calculate the average output 
-        % value f0(i) = f0(i) + outputs(j,i)/n_sim;
-        % The TOTAL VARIANCE of Model Outputs is estimated by squaring all 
-        % outputs for an MC simulation then taking the average of squared
-        % Model Outputs. Call this value D. The total variance is defined 
-        % as D minus the square of the average model outputs, D - f0^2
-        D(i) = D(i) + outputs(j,i).^2/n_sim;
+        % The average of the Model Outputs is defined as the sum of all
+        % model outputs for each output in fx, divided by the number of
+        % Monte Carlo Simulations
+        f0(i) = f0(i) + fx(j,i);
     end
+    f0(i) = f0(i)/n_sim;
 end
 
 % (c) Estimating Total Output Variance, D, using MC Model Outputs
 for i = 1:n_out
-    D(i) = D(i) - f0(i).^2;
+    for j = 1:n_sim
+        % The TOTAL variance of Model Outputs is defined using ANOVA, and
+        % is the integral of the square of function outputs minus the
+        % square of the average of the function outputs. This integral is
+        % estimated numerically via Monte Carlo as follows:
+        D(i) = D(i) + ((fx(j,i)^2)-(f0(i)^2));
+    end
+    D(i) = D(i)/n_sim;
 end
 
 % (d) Compute Partial Variances (1st Order Sobol)
 % The Partial Variances for each Uncertain Parameter j is defined as the 
-% Total Model Output Variance D minus (1/2*simulation#) times the square 
-% of Monte Carlo Outputs with all parameters samples from Parameter Space 
-% minus the Monte Carlo Outputs with Parameter j sampled from Parameter 
-% Space and non-j sampled from Complementary Space.
+% total variance D minus (1/2N) times the square of (fx - fx_P)
 D_1st = zeros(n_kp, n_out);
-F_1st = 0;                                 % Initialize 1st Order Factors
+F_Par = zeros(n_kp, n_out);           % Initialize Partial Variance Factors
 for i = 1:n_kp
     for j = 1:n_out
         for k = 1:n_sim
-        % D_1st is a n_kp x n_out vector. The values for Key Params are 
-        % initialized to D(n_out) according to the CCU Model Outputs. For 
-        % each MC simulation, output(k,j)-eval_P(k,j,i) is squared, then 
-        % normalized by dividing by [2*(n_sim)]. As we loop through n_sim, 
-        % the Normalized Squared Difference for Key Param j is iteratively 
-        % subtracted from the Total Model Output Variance D. When we finish 
-        % subtracting the differences after looping through all n_sim, the 
-        % Residual Variance is the Partial Variance for that parameter
-        F_1st = F_1st + ((outputs(k,j) - eval_P(k,j,i)).^2/(2*n_sim));
+            F_Par(i,j) = F_Par(i,j) + (fx(k,j) - fx_P(k,j,i))^2;
         end
-        D_1st(i,j) = D(j) - F_1st;
-        F_1st = 0;
+        D_1st(i,j) = D(j) - (F_Par(i,j)/(n_sim*2));
     end
 end
 
 %(e) Compute Total Variances (Total Sobol)
-D_Total = zeros(n_kp, n_out);
-F_Total = 0;                               % Initialize Total Sobol Factors
+D_Tot = zeros(n_kp, n_out);
+F_cPar = zeros(n_kp, n_out);          % Initialize Total Sobol Factors
 for i = 1:n_kp
     for j = 1:n_out
         for k = 1:n_sim
-        % D_Total is an n_kp x n_out vector that is initialized to zero. 
-        % Unlike 1st order Sobol, the parameter of interest is sampled from 
-        % the Complementary Space. For each MC simulation, the differences
-        % in output(k,j) and eval_C(k,j,i) are squared, then normalized by 
-        % dividing by [2*(n_sim)]. As we loop through n_sim, the Normalized 
-        % Square Differences for KeyParam j is iteratively added. The total 
-        % sum after iterative adding is the Total Variance contribution of 
-        % Key Param j.
-        F_Total = F_Total + ((outputs(k,j) - eval_C(k,j,i)).^2/(2*n_sim));
+            % Unlike 1st order Sobol, the parameter of interest is sampled
+            % from the Complementary Space. For each MC simulation, fx(k,j)
+            % - fx_C(k,j,i) is squared then normalized by dividing by 
+            % 2(n_sim). As we loop through n_sim, the Normalized Square 
+            % Differences for KeyParam jis iteratively added
+            F_cPar(i,j) = F_cPar(i,j) + (fx(k,j) - fx_C(k,j,i))^2;
         end
-        D_Total(i,j) = D_Total(i,j) + F_Total;
-        F_Total = 0;
+        D_Tot(i,j) = F_cPar(i,j)/(n_sim*2);
     end
 end
 %%Progress Bar%%
 waitbar(950/total_steps, progress, 'Exporting Sobol Indices');
 
 
+
 %% [5] Determine Sobol Indices
-% (a) Compute 1st Order and Total Sobol Indices
-% For 1st Order Sobol, for each CCU model output, its D_1st is divided by D
-% (total output variance). The sum of 1st Order Sobol should equal 1 for 
-% the Conditional Independence Assumption to hold, however, should the sum 
-% (either 1st Order or Total) be >1, it means that parameter correlations 
-% are present. The greater the sum is to 1, the greater the parameter 
-% correlations and the less the conditional independence assumption holds
+% (a) Compute Sum of Partial and Total Variances
+% The Sum of Partial Variances is computed by adding D_1st/D for each
+% particular output j. The Sum of Total Variances is computed by adding
+% D_Tot/D for each output j. Note, if the Naive assumption for Sobol Method
+% truly holds, the sum of all D_1st/D should not exceed 1. However, if
+% the model parameters are positively/negatively correlated, the sum of 
+% partial and total indices can be >1 and <1, respectively.
+Sum_Partial = zeros(n_out);
+Sum_Total = zeros(n_out);
+for i = 1:n_out
+    Sum_Partial(i) = sum(D_1st(:,i));
+    Sum_Total(i) = sum(D_Tot(:,i));
+end
+
+% (b) Compute Rank Scores based on Partial/Total Variances
+% The closer the sum of partial variances is to the total model output
+% variances, the more additive the model is. 
 Sobol_1st = zeros(n_kp, n_out);
 Sobol_Total = zeros(n_kp, n_out);
 for i = 1:n_out
     for j = 1:n_kp
-    Sobol_1st(j,i) = D_1st(j,i)/D(i);
-    Sobol_Total(j,i) = D_Total(j,i)/D(i);
+        Sobol_1st(j,i) = D_1st(j,i)/D(i);
+        Sobol_Total(j,i) = D_Tot(j,i)/D(i);
     end
 end
 
 % (b) Rank the Parameters via Sobol Indices
 for i = 1:n_out
-    % 1st Order Indices
+    % 1st Order Sobol Indices
     [score, rank] = sort(Sobol_1st(:,i), 'descend');
     fprintf('1st Order Sobol Ranks for CCU Eval Output %.4f \n', i)
     rank
-    sprintf('The Rank Scores for the Above Order are: ')
+    sprintf('The Partial Variance Indices (1st Order Sobol) for the Above Order are: ')
     Sobol_1st(:,i)
-    fprintf('Sum of 1st Order Sobol Indices for Output %.4f \n', i)
+    fprintf('Sum of Partial Variance Indices for Output %.4f \n', i)
     sum(Sobol_1st(:,i))
+    fprintf('Relative 1st Order Sobol Indices for Output %.4f \n', i)
+    Sobol_1st(:,i)*D(i)/Sum_Partial(i)
     % Total Sobol Indices
     [score2, rank2] = sort(Sobol_Total(:,i), 'descend');
     fprintf('Total Sobol Ranks for CCU Eval Output %.4f \n', i)
     rank2
-    sprintf('The Rank Scores for the Above Order are: ')
+    sprintf('The Total Variance Indices (Total Sobol) for the Above Order are: ')
     Sobol_Total(:,i)
     fprintf('Sum of Total Sobol Indices for CCU Eval Output %.4f \n', i)
     sum(Sobol_Total(:,i))
+    fprintf('Relative Total Sobol Indices for Output %.4f \n', i)
+    Sobol_Total(:,i)*D(i)/Sum_Total(i)
 end
+
 
 
 %% [6] Plot and Export Results
@@ -289,25 +312,25 @@ nbins = 30;             % Define resolution of output metric's historgram
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Output #1
     figure('Name', 'Variance of Unit Prod Cost')
-    histogram(outputs(:,1), nbins, 'facecolor', [0, 0, 0])
+    histogram(fx(:,1), nbins, 'facecolor', [0, 0, 0])
     xlabel('Biocrude Unit Prod. Cost, $/kg')
     ylabel('Frequency')
     % Output #2
     figure('Name', 'Variance of Specific GWI')
-    histogram(outputs(:,2), nbins, 'facecolor', [0, 0.5, 0])
+    histogram(fx(:,2), nbins, 'facecolor', [0, 0.5, 0])
     xlabel('Specific GWI, kg-CO2-eq/kg')
     ylabel('Frequency')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % (c) Export Results
-xlswrite('MC-Sobol_ParameterSpace.xls', ParSpace)
-xlswrite('MC-Sobol_ComplementarySpace.xls', c_ParSpace)
-xlswrite('MC-Sobol_EvaluatedOutputs.xls', outputs)
-xlswrite('MC-Sobol_TotVar+F0.xls', [D, f0])
-xlswrite('MC-Sobol_Variance_1st.xls', D_1st)
-xlswrite('MC-Sobol_Variance_Total.xls', D_Total)
-xlswrite('MC-Sobol_1stOrderSobol.xls', [score, rank])
-xlswrite('MC-Sobol_TotalSobol.xls', [score2, rank2])
+if export == 1
+    xlswrite('MC-Sobol_EvaluatedOutputs.xls', fx)
+    xlswrite('MC-Sobol_TotVar+F0.xls', [D, f0])
+    xlswrite('MC-Sobol_Variance_1st.xls', D_1st)
+    xlswrite('MC-Sobol_Variance_Total.xls', D_Tot)
+    xlswrite('MC-Sobol_1stOrderSobol.xls', [score, rank])
+    xlswrite('MC-Sobol_TotalSobol.xls', [score2, rank2])
+end
 %%Progress Bar%%
 waitbar(1000/total_steps, progress, 'Complete');
 delete(progress)
